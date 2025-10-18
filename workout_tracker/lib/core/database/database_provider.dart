4b20b839ecd53654d81a9d9e6516762ea6a3dc40
abhirham <savarav.a@gmail.com>
2025-10-18 10:44:24 -0400

    wip

--- workout_tracker/lib/core/database/database_provider.dart
+++ workout_tracker/lib/core/database/database_provider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app_database.dart';
-import 'database_seeder.dart';
 import '../../features/workouts/data/workout_alternative_repository.dart';
 import '../../features/workouts/data/completed_set_repository.dart';
 import '../../features/workouts/data/workout_template_repository.dart';
@@ -28,11 +27,6 @@ final completedSetRepositoryProvider = Provider<CompletedSetRepository>((ref) {
 final workoutTemplateRepositoryProvider = Provider<WorkoutTemplateRepository>((ref) {
   final database = ref.watch(databaseProvider);
   return WorkoutTemplateRepository(database);
-});
-
-final databaseSeederProvider = Provider<DatabaseSeeder>((ref) {
-  final database = ref.watch(databaseProvider);
-  return DatabaseSeeder(database);
 });
 
 final dayRepositoryProvider = Provider<DayRepository>((ref) {
